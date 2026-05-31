0ecb2f4e3bb135b23ecccb443235837f4a13ad10
Sofia Diaz <sofiadiazs02@gmail.com>
2026-05-30 21:52:13 -0500

    feat: add FastAPI backend database connection and ORM task model

---
+++ ToDoAPI/models.py
@@ -0,0 +1,12 @@
+# Modelo de la Base de Datos.
+
+from sqlalchemy import Column, Integer, String, Boolean
+from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    completed = Column(Boolean, default=False)
